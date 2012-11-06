ff64ff1e5e047f0d190789b3191470f7d37f6d92
Churchill Lee <feliciousx@gmail.com>
2012-11-06 08:40:47 +0800

    ~user\index.php
    Rough skeleton

--- user/index.php
+++ user/index.php
@@ -2,3 +2,14 @@
 header('Location: ../index.php');
 exit;
 ?>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+	<!-- Banner -->
+	<!-- Navigation Pane -->
+	<!-- Content -->
+	
+</body>
+</html>
