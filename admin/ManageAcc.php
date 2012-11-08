586308effbbec41687e803eac07f104871682c3a
Jeffrey Chiong Kung Hui <jeffrey_ckh@hotmail.com>
2012-11-08 15:24:36 +0800

    trail
    XD

---
+++ admin/ManageAcc.php
@@ -0,0 +1,43 @@
+<?php
+	require_once('../view/ManageAccView.php');
+	$ManageAccView = new ManageAccView();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>
+        	Manage Students
+        </title>
+ </head>
+ <body>
+ 	<h2>Manage Students</h2>
+ 	<h2>Search</h2>
+ 	 	<form action="MangageAcc.php" method="post">
+      		 <fieldset class="form">
+	             <label class="form">Search:</label>
+	             <input type="text" name="searchItem" />
+	             <select name="type">
+	            	<option value="studentID" selected="selected">Student ID</option>
+	              	<option value="studentFirstName">Firs tName</option>
+	              	<option value="studentLastName">Last Name</option>
+	              </select>
+	              <input class="form" type="submit" name="showAll" value="Show All" />
+	               <input class="form" type="submit" name="search" value="Search" />
+              </fieldset>
+     </form> 
+ 	<h2>Account</h2>
+ 	<?php
+        $ManageAccView->activate();
+        $ManageAccView->deactivate();
+        $ManageAccView->delete();
+        if (isset($_POST['search'])) {
+            $ManageAccView->search();
+        } else {
+         	$ManageAccView->showStudent();
+      	}
+     ?>
+
+
+
---
+++ view/ManageAccView.php
@@ -0,0 +1,123 @@
+<?php
+
+require_once('../inc/config.php');
+require_once('../controller/studentcontroller.php');
+require_once('../model/studentmodel.php');
+
+class ManageAccView{
+
+	public function search(){
+		if(isset($_POST['search'])){
+			try{
+				$studentController = new StudentController();
+				$sResult = array();
+
+				if(strcmp($_POST['type'],'StudentID')==0){
+					$sResult = $userController -> getAllStudentByID($_POST['searchItem']);	
+				}
+				elseif(strcmp($_POST['type'],'StudentFirstName')==0){
+					$sResult = $userController->getByFirstName($_POST['searchItem']);
+				}
+				else{
+					throw new Exception('Invalid search criteria');
+				}
+
+				if(sizeof($sResult)> 0){
+					echo'<table>';
+					echo'<thead><tr><th>Student ID</th>First Name</th><th>Last Name</th><th>Account Status</th><th></th><th></th></tr></thead>';
+					echo'<tbody>';
+					for($i = 0;$i<sizeof($sResult);$i++){
+					echo'<tr>';
+					echo'<form action = "ManageAcc.php" method = "post">';
+					echo '<td><input type="text" value="' . $sResult[$i]['StudentID'] . '" name="studentID" readonly="readonly" /></td>';
+                    echo '<td>' . $sResult[$i]['StudentFirstName'] . '</td>';
+                    echo '<td>' . $sResult[$i]['StudentLastName'] . '</td>';
+                    echo '<td' . ($sResult[$i]['ActivationStatus'] == 1 ? ' class="success_message">Activated' : ' class="error_message">Not Activated') . '</td>';
+                    echo '<td><input onclick="confirmDialog();" type="submit" value="' . ($searchResult[$i]['ActivationStatus'] == 1 ? 'Deactivate" name="deactivate"' : 'Activate" name="activate"') . ' /></td>';
+                    echo '<td><input onclick="confirmDialog();" type="submit" name="delete" value="Remove" /></td>';
+                    echo '</form>';
+                    echo '</tr>';
+                    }
+                    echo '</tbody>';
+                    echo '</table>';
+                } 
+                else {
+                    echo '<p>No students in the database.</p>';
+                }
+            	}catch (Exception $e) {
+                echo '<p class="error_message">' . $e->getMessage() . '</p>';
+
+
+					}
+				}
+			}
+	public function showStudent() {
+        $studentController = new StudentController();
+        $sList = $studentController->getAllStudent();
+
+        if (sizeof($sList) > 0) {
+            echo '<table>';
+            echo '<thead><tr><th>Student ID</th><th>First Name</th><th>Last Name</th><th>Account Status</th><th></th><th></th></tr></thead>';
+            echo '<tbody>';
+            for ($i = 0; $i < sizeof($sList); $i++) {
+                echo '<tr>';
+                echo '<form action="manage_account.php" method="post">';
+                echo '<td><input type="text" value="' . $sList[$i]['StudentID'] . '" name="studentID" readonly="readonly" /></td>';
+                echo '<td>' . $sList[$i]['StudentFirstName'] . '</td>';
+                echo '<td>' . $sList[$i]['StudentLastName'] . '</td>';
+                echo '<td' . ($sList[$i]['ActivationStatus'] == 1 ? ' class="success_message">Activated' : ' class="error_message">Not Activated') . '</td>';
+                echo '<td><input onclick="confirmDialog();" type="submit" value="' . ($sList[$i]['ActivationStatus'] == 1 ? 'Deactivate" name="deactivate"' : 'Activate" name="activate"') . ' /></td>';
+                echo '<td><input onclick="confirmDialog();" type="submit" name="delete" value="Remove" /></td>';
+                echo '</form>';
+                echo '</tr>';
+            }
+            echo '</tbody>';
+            echo '</table>';
+        } else {
+            echo '<p>No students in the database.</p>';
+        }
+    }
+		
+    public function activate(){
+    	if(isset($_POST['active'])){
+    		try{
+    			$studentController = new StudentController();
+    			$studentController->activerStudent($_POST['studentID']);
+
+    			echo'<p class ="success_message">Account actived.</p>';
+
+    		  }
+            catch (Exception $e){
+    			echo'<p class="error_message">'.$e->getMessage().'</p>';
+    		}
+    	}
+    }
+
+    public function deactivate() {
+        if (isset($_POST['deactivate'])) {
+            try {
+               $studentController = new StudentController();
+                $studentController->deactivateUser($_POST['studentID']);
+
+                echo '<p class="success_message">Account deactivated.</p>';
+            } catch (Exception $e) {
+                echo '<p class="error_message">' . $e->getMessage() . '</p>';
+            }
+        }
+    }
+
+    public function delete(){
+    	if(isset($_POST['delete'])){
+    		try{
+    			$studentController = new StudentController();
+    			$studentController->removeStudent($_POST['studentID']);
+    			echo'<p class="success_message">Account deleted.</p>';
+    		}catch (Exception $e){
+    			echo'<p class = "error_message">'.$e->getMessage().'</p>';
+    		}
+    	}
+    }
+}
+
+
+?>
