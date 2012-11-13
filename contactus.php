1899fbf25de27b60964686a8b65892f6ac150348
Jeffrey Chiong Kung Hui <jeffrey_ckh@hotmail.com>
2012-11-13 09:47:30 +0800

    Contact Us
    what should i add in the page?

---
+++ contactus.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>Contact Us</title>
+     </head>
+     <body>
+     	<h2>Contact Us</h2>
+     	<table border="1" bordercolor = grey>
+     		<tr>
+     			<th>Name</th>
+     			<th>Email</th>
+     			<th>Contact Number</th>
+     		</tr>
+     		<tr>
+     			<td>Churchill</td>
+     			<td>xxx@gmail.com</td>
+     			<td>0165555555</td>
+     		</tr>
+     		<tr>
+     			<td>Isaac</td>
+     			<td>xxx@gmail.com</td>
+     			<td>0165555555</td>
+     		</tr>
+     		<tr>
+     			<td>Liew</td>
+     			<td>xxx@gmail.com</td>
+     			<td>0165555555</td>
+     		</tr>
+     		<tr>
+     			<td>Jeffrey</td>
+     			<td>xxx@gmail.com</td>
+     			<td>0165555555</td>
+     		</tr>
+     	</body>
+</html>
